f4791ccfda4a9d56a9b532fa7f83c616e52e3152
w4570 <35764481+w4570@users.noreply.github.com>
2022-11-29 23:04:11 +0800

    Update to match Stable

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
-	HOMEWORK: PHYSICS ENGINE PHASE 2
-	MEMBERS: Junma Ogawa
-			 Julian Urrutia
+HOMEWORK: PHYSICS ENGINE PHASE 2
+MEMBERS : Junma Ogawa
+Julian Urrutia
 */
 
 //-------------------- INCLUDES --------------------\\
@@ -29,12 +29,68 @@
 #include <glm/gtx/norm.hpp>
 using namespace glm;
 
+struct Particle {
+	glm::vec3 pos, speed;
+	int life;
+	float size, angle, weight;
+	float cameradistance; // *Squared* distance to the camera. if dead : -1.0f
+
+	bool operator<(const Particle& that) const {
+		// Sort in reverse order : far particles drawn first.
+		return this->cameradistance > that.cameradistance;
+	}
+};
+
+//-------------------- VARIABLES --------------------\\
+
 GLFWwindow* window;
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
+const int MaxParticles = 10;
+Particle ParticlesContainer[MaxParticles];
+int LastUsedParticle = 0;
+
+glm::vec3 position = glm::vec3(0, 0, 5);	// Initial Camera Position
+float horizontalAngle = 3.14f;				// Initial vertical angle : Z axis (3.14)
+float verticalAngle = 0.0f;					// Initial vertical angle : none
+float initialFoV = 45.0f;					// Initial Field of View
+float speed = 3.0f;							// Camera Movement Speed (3 units / second)
+float mouseSpeed = 0.005f;					// Camera Rotation Speed
+
+//-------------------- FUNCTIONS --------------------\\
+
+float getDistance(float X_POS_1, float Y_POS_1, float Z_POS_1, float X_POS_2, float Y_POS_2, float Z_POS_2) {
+	return sqrt(pow(X_POS_2 - X_POS_1, 2) + pow(Y_POS_2 - Y_POS_1, 2) + pow(Z_POS_2 - Z_POS_1, 2));
+};
+
+int FindUnusedParticle() {
+
+	for (int i = LastUsedParticle; i < MaxParticles; i++) {
+		if (ParticlesContainer[i].life < 0) {
+			LastUsedParticle = i;
+			return i;
+		}
+	}
+
+	for (int i = 0; i < LastUsedParticle; i++) {
+		if (ParticlesContainer[i].life < 0) {
+			LastUsedParticle = i;
+			return i;
+		}
+	}
+
+	return 0; // All particles are taken, override the first one
+}
+
+void SortParticles() {
+	std::sort(&ParticlesContainer[0], &ParticlesContainer[MaxParticles]);
+}
 
 int main() {
 
-// Pragma Region: Initialization
-// Description: Initializing window creation using GLFW and GLEW libraries
+	// Pragma Region: Initialization
+	// Description: Initializing window creation using GLFW and GLEW libraries
 #pragma region Initialization
 
 	// Initialise GLFW
@@ -42,7 +98,7 @@ int main() {
 		fprintf(stderr, "Failed to initialized! \n");
 		return -1;
 	}
-	
+
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -51,7 +107,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow(1200, 800, "Physics Engine Phase 2 (Ogawa & Urrutia)", NULL, NULL);
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Physics Engine Phase 2 (Ogawa & Urrutia)", NULL, NULL);
 	if (window == NULL) {
 		fprintf(stderr, "Failed to load window! \n");
 		return -1;
@@ -72,7 +128,12 @@ int main() {
 	ObjData bullet;
 	LoadObjFile(&bullet, "Sphere.obj");
 	GLfloat bulletOffsets[] = { 0.0f, 0.0f, 0.0f };
-	LoadObjToMemory(&bullet, 1.0f, bulletOffsets);
+	LoadObjToMemory(&bullet, 0.1f, bulletOffsets);
+
+	ObjData box;
+	LoadObjFile(&box, "Box.obj");
+	GLfloat boxOffsets[] = { 0.0f, 0.0f, 0.0f };
+	LoadObjToMemory(&box, 0.2f, boxOffsets);
 
 #pragma endregion
 
@@ -101,9 +162,13 @@ int main() {
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	// Get mouse position
+	double xpos, ypos;
+	glfwGetCursorPos(window, &xpos, &ypos);
+
 	// Set the mouse at the center of the screen
 	glfwPollEvents();
-	glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+	glfwSetCursorPos(window, SCR_WIDTH / 2, SCR_HEIGHT / 2);
 
 	// Setup White Background
 	glClearColor(0.9f, 0.9f, 0.9f, 0.0f);
@@ -117,28 +182,95 @@ int main() {
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
+	// glfwGetTime is called only once, the first time this function is called
+	static double prevTime = glfwGetTime();
+
+	// Compute time difference between current and last frame
+	double currentTime = glfwGetTime();
+	float deltaTime = float(currentTime - prevTime);
+
 	while (!glfwWindowShouldClose(window)) {
+
+#pragma region Viewport
+		float ratio;
+		int width, height;
+
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+
+		glViewport(0, 0, width, height);
+
+#pragma endregion
+
+#pragma region Projection
+
+		// Perspective Projection
+		projection = glm::perspective(glm::radians(90.0f), ratio, 0.1f, 10.0f),
+
+			// Set projection matrix in shader
+			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+#pragma endregion
+
+#pragma region View
+
+		// Compute new orientation
+		horizontalAngle += mouseSpeed * float(SCR_WIDTH / 2 - xpos);
+		verticalAngle += mouseSpeed * float(SCR_HEIGHT / 2 - ypos);
+
+		// Restriction Camera Angle Vertical
+		if (verticalAngle > 0.8f) {
+			verticalAngle = 0.8f;
+		}
+
+		if (verticalAngle < -0.8f) {
+			verticalAngle = -0.8f;
+		}
+
+		glm::mat4 view = glm::lookAt(
+			glm::vec3(0.5f, 0.0f, -1.0f),
+			glm::vec3(trans[3][0], trans[3][1], trans[3][2]),
+			glm::vec3(0.0f, 1.0f, 0.0f)
+		);
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+#pragma endregion
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//toggle to render wit GL_FILL or GL_LINE
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 #pragma region Draw
 
 		//Drawing the Bullet
 		glBindVertexArray(bullet.vaoId);
 
-		//Adjust Bullet Position, Size, & Color
+		trans = glm::mat4(1.0f); // identit
+		trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, 1.0f));
+		trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
+
+		glActiveTexture(GL_TEXTURE0);
+		GLuint bulletTexture = bullet.textures[bullet.materials[0].diffuse_texname];
+		glBindTexture(GL_TEXTURE_2D, bulletTexture);
+
+		glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+		glDrawElements(GL_TRIANGLES, bullet.numFaces, GL_UNSIGNED_INT, (void*)0);
+
+		////Drawing the Box
+		glBindVertexArray(box.vaoId);
+
 		trans = glm::mat4(1.0f); // identity
-		trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, -3.0f));
-		trans = glm::scale(trans, glm::vec3(1.0f, 1.0f, 1.0f));
-		glUniform3f(colorLoc, 0.2f, 0.2f, 0.2f);
-		
+		trans = glm::translate(trans, glm::vec3(0.0f, 0.0f, 0.0f));
+		trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
+
+		glActiveTexture(GL_TEXTURE0);
+		GLuint boxTexture = box.textures[box.materials[0].diffuse_texname];
+		glBindTexture(GL_TEXTURE_2D, boxTexture);
+
 		//Send to shader
 		glUniformMatrix4fv(modelTransformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
-		glDrawElements(GL_TRIANGLES, bullet.numFaces, GL_UNSIGNED_INT,(void*)0);
+		glDrawElements(GL_TRIANGLES, box.numFaces, GL_UNSIGNED_INT, (void*)0);
 
 #pragma endregion
 
